529933559f0a65ae5ca990cbc275980da18d8542
George Winston K <136403897+georgewin18@users.noreply.github.com>
2023-06-13 09:23:21 +0700

    Add files via upload

---
+++ Notes/app/src/main/java/com/georgewk/notes/entity/Note.kt
@@ -0,0 +1,10 @@
+package com.georgewk.notes.entity
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Note(var id: Int = 0,
+                var title: String? = null,
+                var description: String? = null,
+                var date: String? = null ) : Parcelable
---
+++ Notes/app/src/main/java/com/georgewk/notes/helper/MappingHelper.kt
@@ -0,0 +1,21 @@
+package com.georgewk.notes.helper
+
+import android.database.Cursor
+import com.georgewk.notes.db.DatabaseContract
+import com.georgewk.notes.entity.Note
+
+object MappingHelper {
+    fun mapCursorToArrayList(notesCursor: Cursor?): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        notesCursor?.apply {
+            while (moveToNext()) {
+                val id = getInt(getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+                val title = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+                val description = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+                val date = getString(getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+                notesList.add(Note(id, title, description, date))
+            }
+        }
+        return notesList
+    }
+}
